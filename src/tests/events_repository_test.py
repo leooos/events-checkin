20897a4ddb777f26d0ef058fbea13de1d28c556a
leooos <leokagohara@gmail.com>
2024-04-06 21:57:28 -0300

    Add tests folder and Events integration tests

---
+++ src/tests/events_repository_test.py
@@ -0,0 +1,24 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from src.models.repository.events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip("novo registro em banco de dados")
+def test_insert_event():
+    event = {
+        "uuid": "meu-uuid-e-nois",
+        "title": "meu title",
+        "slug": "meu-slug-aqui!",
+        "maximum_attendees": 20
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+    
+def test_get_event_by_id():
+    event_id = "meu-uuid-e-nois"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
